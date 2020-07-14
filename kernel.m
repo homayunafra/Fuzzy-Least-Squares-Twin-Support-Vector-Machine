0a5b0fce0c3c3c32a599829ef39df893c0c628fa
Homayun <homayun.it@gmail.com>
2020-07-14 23:59:22 +0300

    some functions are removed and replaced with modified version!

--- kernel.m
+++
@@ -1,9 +0,0 @@
-function [ K ] = kernel( type, Xi, Xj )
-	if strcmp(type,'RBF')
-		sigma=1;
-		K=exp(norm(Xi-Xj)/(2*sigma^2));
-	else if strcmp(type,'Poly')
-		p=3;
-		K=(1+Xi*transpose(Xj))^p;
-	end
-end
